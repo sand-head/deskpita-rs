ff44cf1efb8e8cc104b19a582c996d59105250a2
Justin Schweigert <justin@schweigert.dev>
2022-02-21 19:39:08 -0600

    Only change the fan speed if it actually changes

--- src/services.rs
+++ src/services.rs
@@ -5,6 +5,7 @@ use once_cell::sync::Lazy;
 use tokio::{
   fs::File,
   io::{AsyncReadExt, AsyncWriteExt},
+  time,
 };
 use tokio_serial::SerialStream;
 
@@ -26,27 +27,49 @@ pub enum ServiceCommands {
 }
 
 pub async fn sync_fan_speed_with_cpu_temp(port: &mut SerialStream) -> anyhow::Result<()> {
-  loop {
-    let data = GLOBAL_DATA.lock().unwrap();
-    let cpu_temp = get_cpu_temp().await?;
+  let mut current_speed = -1;
 
-    let mut new_speed = 0;
-    for (temp, speed) in data.iter() {
-      if cpu_temp >= *temp {
-        new_speed = *speed;
-        break;
-      }
+  loop {
+    let cpu_temp = get_cpu_temp().await?;
+    let new_speed = get_new_speed(cpu_temp) as i32;
+
+    if new_speed != current_speed {
+      println!("CPU temp: {}°C, fan speed: {}%", cpu_temp, new_speed);
+      fan::set_fan_speed(port, new_speed).await?;
+      current_speed = new_speed;
     }
 
-    println!("CPU temp: {}°C, fan speed: {}%", cpu_temp, new_speed);
-    fan::set_fan_speed(port, new_speed).await?;
-    tokio::time::sleep(Duration::from_secs(10)).await;
+    time::sleep(Duration::from_secs(10)).await;
   }
 }
 
 pub async fn send_power_off(port: &mut SerialStream) -> anyhow::Result<()> {
   port.write(b"power_off").await?;
   Ok(())
+}
+
+fn get_new_speed(cpu_temp: i32) -> f64 {
+  let data = GLOBAL_DATA.lock().unwrap();
+  if data.contains_key(&cpu_temp) {
+    return data[&cpu_temp].into();
+  }
+
+  let mut upper_point = (0, 0);
+  let mut lower_point = (0, 0);
+
+  // get the points that bracket the current CPU temp
+  for (temp, speed) in data.iter() {
+    if cpu_temp > *temp {
+      lower_point = (*temp, *speed);
+    } else {
+      upper_point = (*temp, *speed);
+    }
+  }
+
+  // calculate the new speed by interpolating between the two points
+  let slope = (upper_point.1 - lower_point.1) as f64 / (upper_point.0 - lower_point.0) as f64;
+  let y_intercept = upper_point.1 as f64 - slope * upper_point.0 as f64;
+  slope * cpu_temp as f64 + y_intercept
 }
 
 async fn get_cpu_temp() -> anyhow::Result<i32> {
